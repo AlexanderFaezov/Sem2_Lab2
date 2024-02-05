f5ef7584589d685e18204c94cac161f0995bb7a4
AlexanderFaezov <s.faezov.com@mail.ru>
2024-02-05 19:14:44 +0500

    Добавьте файлы проекта.

---
+++ Sem2_Lab2/Sem2_Lab2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    using namespace std;
+
+    const int n = 7;
+    int a[n];
+    int temp;
+    int p = 1, q = n - 2, i = p, j = q;
+
+    srand(time(0));
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = rand() % 101 - 50;
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    while (i < j)
+    {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+        i++;
+        j--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
